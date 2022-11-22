1eb72e2dcad968d8bb0b974d84f6b80f07376685
alex-h <alexanderohernandez21@gmail.com>
2022-11-22 13:40:40 -0800

    classes

--- src/main/java/person/Adult.java
+++ src/main/java/person/Adult.java
@@ -1,4 +1,29 @@
 package person;
 
 public class Adult {
+    private int libraryCardNumber;
+    private int booksCheckedOut;
+
+    public Adult(String firstName, String lastName, int age,
+                 String preference, int libraryCardNumber, int booksCheckedOut){
+        super();
+        libraryCardNumber = libraryCardNumber;
+        booksCheckedOut = booksCheckedOut;
+    }
+
+    public int getBooksCheckedOut() {
+        return this.booksCheckedOut;
+    }
+    public int getLibraryCardNumber(){
+        return this.libraryCardNumber;
+    }
+
+    public void setLibraryCardNumber(){
+        this.libraryCardNumber = libraryCardNumber;
+    }
+
+    public void setBooksCheckedOut(){
+        this.booksCheckedOut = booksCheckedOut;
+    }
+
 }
--- src/main/java/person/Person.java
+++ src/main/java/person/Person.java
@@ -4,11 +4,13 @@ public class Person {
     private String firstName;
     private String lastName;
     private int age;
+    private String preference;
 
-    public Person(String firstName, String lastName, int age){
+    public Person(String firstName, String lastName, int age, String preference){
         firstName = firstName;
         lastName = lastName;
         age = age;
+        preference = preference;
     }
 
     public String getFirstName() {
@@ -19,6 +21,9 @@ public class Person {
         return this.lastName;
     }
 
+    public String getPreference(){
+        return this.preference;
+    }
     public int getAge(){
         return this.age;
     }
@@ -34,4 +39,5 @@ public class Person {
     public void setAge(int age){
         this.age = age;
     }
+    public void setPreference(String preference){ this.preference = preference;}
 }
