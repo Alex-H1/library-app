dbbe831fb7c39d5f50751a6c8287463644eb4d40
alex hernandez <alexanderohernandez21@gmail.com>
2022-11-22 19:37:39 -0800

    troubleshooting

--- src/main/java/person/Person.java
+++ src/main/java/person/Person.java
@@ -40,4 +40,27 @@ public class Person {
         this.age = age;
     }
     public void setPreference(String preference){ this.preference = preference;}
+
+    public void main(String[] args) {
+        Person p1 = new Person("Alex", "h", 19, "science");
+        Person p2 = new Person("Alex", "h", 19, "science");
+        System.out.println(p1);
+    }
+
+//    @Override
+    public boolean equals(Person p1){
+        if(p1 == this){
+            return true;
+        }
+        if(!(p1 instanceof Person)){
+            return false;
+        }
+        Person p2 = (Person)p1;
+        boolean firstNameEquals = (this.firstName == null && p2.firstName == null) ||
+                (this.firstName != null && this.firstName.equals(p2.firstName));
+        return this.lastName == p2.lastName && firstNameEquals;
+    }
+
+
+
 }
