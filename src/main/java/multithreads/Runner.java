425311076e8f18944d9069de37ed949577321928
alex-h <alexanderohernandez21@gmail.com>
2022-12-28 12:02:17 -0800

    iFuture and CompletableFuture

--- src/main/java/multithreads/Runner.java
+++ src/main/java/multithreads/Runner.java
@@ -3,14 +3,12 @@ package multithreads;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.*;
 
 public class Runner {
     public final static Logger LOG = LogManager.getLogger(Runner.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 //        for (int i = 0; i < 5; i++) {
 //            MultiThreadRun thread = new MultiThreadRun();
 ////            thread.start();
@@ -18,19 +16,22 @@ public class Runner {
 //            thread1.start();
 //        }
 
-        ExecutorService fixedPool = Executors.newFixedThreadPool(5);
-        ThreadPoolExecutor pool = (ThreadPoolExecutor) fixedPool;
+//        ExecutorService fixedPool = Executors.newFixedThreadPool(5);
+//        ThreadPoolExecutor pool = (ThreadPoolExecutor) fixedPool;
+//
+//        LOG.info("Core threads: " + ((ThreadPoolExecutor) fixedPool).getCorePoolSize());
+//        LOG.info("Max pool size: " + ((ThreadPoolExecutor) fixedPool).getMaximumPoolSize());
+//        LOG.info("Total task count: " + ((ThreadPoolExecutor) fixedPool).getTaskCount());
+//        for(int i = 0; i <= 7; i++){
+//            fixedPool.submit(new MultiThreadRun());
+//        }
+//        LOG.info("Core threads: " + ((ThreadPoolExecutor) fixedPool).getCorePoolSize());
+//        LOG.info("Max pool size: " + ((ThreadPoolExecutor) fixedPool).getMaximumPoolSize());
+//        LOG.info("Total task count: " + ((ThreadPoolExecutor) fixedPool).getTaskCount());
+//        pool.shutdown();
 
-        LOG.info("Core threads: " + ((ThreadPoolExecutor) fixedPool).getCorePoolSize());
-        LOG.info("Max pool size: " + ((ThreadPoolExecutor) fixedPool).getMaximumPoolSize());
-        LOG.info("Total task count: " + ((ThreadPoolExecutor) fixedPool).getTaskCount());
-        for(int i = 0; i <= 7; i++){
-            fixedPool.submit(new MultiThreadRun());
-        }
-        LOG.info("Core threads: " + ((ThreadPoolExecutor) fixedPool).getCorePoolSize());
-        LOG.info("Max pool size: " + ((ThreadPoolExecutor) fixedPool).getMaximumPoolSize());
-        LOG.info("Total task count: " + ((ThreadPoolExecutor) fixedPool).getTaskCount());
-        pool.shutdown();
+        CompletableFuture<Void> future = CompletableFuture.runAsync(new MultiThreadRun());
+        future.get();
     }
 }
 
